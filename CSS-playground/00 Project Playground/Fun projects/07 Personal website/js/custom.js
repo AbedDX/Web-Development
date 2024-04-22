e157eb7c36c8e71f48e5ee46224e5b79e6b8c410
Abed <abbe_92@live.com>
2024-04-22 14:49:24 +0300

    Personal Porfolio website added

---
+++ CSS-playground/00 Project Playground/Fun projects/07 Personal website/js/custom.js
@@ -0,0 +1,44 @@
+
+(function ($) {
+  
+    "use strict";
+  
+      // NAVBAR
+      $('.navbar-collapse a').on('click',function(){
+        $(".navbar-collapse").collapse('hide');
+      });
+  
+      $(function() {
+        $('.hero-slides').vegas({
+            slides: [
+                { src: 'img/boy_n_cat.jpg' },
+                { src: 'img/art_picture.jpg' },
+                { src: 'img/sunset_view_city.jpg' }
+            ],
+            timer: false,
+            animation: 'kenburns',
+        });
+      });
+      
+      // CUSTOM LINK
+      $('.smoothscroll').click(function(){
+        var el = $(this).attr('href');
+        var elWrapped = $(el);
+        var header_height = $('.navbar').height() + 60;
+    
+        scrollToDiv(elWrapped,header_height);
+        return false;
+    
+        function scrollToDiv(element,navheight){
+          var offset = element.offset();
+          var offsetTop = offset.top;
+          var totalScroll = offsetTop-navheight;
+    
+          $('body,html').animate({
+          scrollTop: totalScroll
+          }, 300);
+        }
+      });
+    
+    })(window.jQuery);
+  
