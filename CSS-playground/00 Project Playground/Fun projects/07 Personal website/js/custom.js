c1323ab0797eaf3c535970a8bb3a184e4a278c36
Abed <abbe_92@live.com>
2024-04-30 12:59:25 +0300

    colour changes and make changes to about section adding Profile picture also sections added projects and contact section and tracking navbar

--- CSS-playground/00 Project Playground/Fun projects/07 Personal website/js/custom.js
+++ CSS-playground/00 Project Playground/Fun projects/07 Personal website/js/custom.js
@@ -1,44 +1,45 @@
 
-(function ($) {
+  (function ($) {
   
-    "use strict";
-  
-      // NAVBAR
-      $('.navbar-collapse a').on('click',function(){
-        $(".navbar-collapse").collapse('hide');
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'img/sunset_view_city.jpg' },
+              { src: 'img/boy_n_cat.jpg' },
+              { src: 'img/art_picture.jpg' }
+          ],
+          timer: false,
+          animation: 'kenburns',
       });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
   
-      $(function() {
-        $('.hero-slides').vegas({
-            slides: [
-                { src: 'img/boy_n_cat.jpg' },
-                { src: 'img/art_picture.jpg' },
-                { src: 'img/sunset_view_city.jpg' }
-            ],
-            timer: false,
-            animation: 'kenburns',
-        });
-      });
-      
-      // CUSTOM LINK
-      $('.smoothscroll').click(function(){
-        var el = $(this).attr('href');
-        var elWrapped = $(el);
-        var header_height = $('.navbar').height() + 60;
-    
-        scrollToDiv(elWrapped,header_height);
-        return false;
-    
-        function scrollToDiv(element,navheight){
-          var offset = element.offset();
-          var offsetTop = offset.top;
-          var totalScroll = offsetTop-navheight;
-    
-          $('body,html').animate({
-          scrollTop: totalScroll
-          }, 300);
-        }
-      });
-    
-    })(window.jQuery);
+      scrollToDiv(elWrapped,header_height);
+      return false;
   
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
