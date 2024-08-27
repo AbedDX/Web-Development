02a8ec5dc549b3cb2ddd74cb33e209d86f5b2b3d
Abed <abbe_92@live.com>
2024-08-27 04:51:57 +0300

    Learning Npm and Node.js

---
+++ 02 JS-playground/06 NPM/index.js
@@ -0,0 +1,12 @@
+// var generateName = require('sillyname');
+
+// import generateName from 'sillyname';
+// var sillyname = generateName();
+
+//console.log(`Hello, my name is ${sillyname}.`);
+
+import superheroes from 'superheroes'
+
+const name = superheroes.random();
+
+console.log(`I'm ${name} and i fight crimes with honor.`);
